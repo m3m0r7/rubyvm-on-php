29ada96c900b158ced53c6f80a247b9fb9786f00
m3m0r7 <memory.style.serv@gmail.com>
2023-07-15 00:40:32 +0900

    update

--- src/VM/Core/Runtime/Version/Ruby3_2/InstructionSequence/InstructionSequenceBody.php
+++ src/VM/Core/Runtime/Version/Ruby3_2/InstructionSequence/InstructionSequenceBody.php
@@ -19,32 +19,34 @@ use RubyVM\VM\Stream\BinaryStreamReader;
 
 class InstructionSequenceBody implements InstructionSequenceBodyInterface
 {
-public function __construct(
-    public readonly int $type,
-    public readonly int $stackMax,
-    public readonly int $iseqSize,
-    public readonly ObjectParameterInterface $objectParam,
-    public readonly int $localTableSize,
-    public readonly int $ciSize,
-    public readonly InsnsInterface $insns,
-    public readonly VariableInterfce $variable,
-    public readonly LocationInterface $location,
-    public readonly int $catchExceptP,
-    public readonly int $builtinInlineP,
-    public readonly int $ivcSize,
-    public readonly int $icvArcSize,
-    public readonly int $iseSize,
-    public readonly int $icSize,
-    public readonly VariableEntries $outerVariables,
-    public readonly VariableEntries $localTable,
-    public readonly CatchEntries $catchTable,
-    public readonly ?InstructionSequenceInterface $parentISeq,
-    public readonly ?InstructionSequenceInterface $localISeq,
-    public readonly ?InstructionSequenceInterface $mandatoryOnlyISeq,
-    public readonly CallInfoEntries $callInfoEntries,
-    public readonly int $bytecodeOffset,
-    public readonly int $bytecodeSize,
-) {
+    public function __construct(
+        public readonly int $type,
+        public readonly int $stackMax,
+        public readonly int $iseqSize,
+        public readonly ObjectParameterInterface $objectParam,
+        public readonly int $localTableSize,
+        public readonly int $ciSize,
+        public readonly InsnsInterface $insns,
+        public readonly VariableInterfce $variable,
+        public readonly LocationInterface $location,
+        public readonly int $catchExceptP,
+        public readonly int $builtinInlineP,
+        public readonly int $ivcSize,
+        public readonly int $icvArcSize,
+        public readonly int $iseSize,
+        public readonly int $icSize,
+        public readonly VariableEntries $outerVariables,
+        public readonly VariableEntries $localTable,
+        public readonly CatchEntries $catchTable,
+        public readonly ?InstructionSequenceInterface $parentISeq,
+        public readonly ?InstructionSequenceInterface $localISeq,
+        public readonly ?InstructionSequenceInterface $mandatoryOnlyISeq,
+        public readonly CallInfoEntries $callInfoEntries,
+        public readonly int $bytecodeOffset,
+        public readonly int $bytecodeSize,
+    )
+    {
+    }
 
 
     public function type(): int
