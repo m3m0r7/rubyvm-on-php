3c9891ab15b8e99475ecf6f1a3bc78e5b7eb2775
m3m0r7 <memory.style.serv@gmail.com>
2023-09-17 13:22:15 +0900

    Add tests

--- tests/Version/Ruby3_2/ClassTest.php
+++ tests/Version/Ruby3_2/ClassTest.php
@@ -37,4 +37,41 @@ class ClassTest extends TestApplication
         $this->assertSame(ExecutedStatus::SUCCESS, $executor->execute()->executedStatus);
         $this->assertSame("Hello World!\n", $rubyVMManager->stdOut->readAll());
     }
+
+    public function testExtendedMethodIntoDefaultProvidingClass(): void
+    {
+        $rubyVMManager = $this->createRubyVMFromCode(
+            <<< '_'
+            class Array
+              def foo(text)
+                puts text
+              end
+            end
+
+            array1 = Array.new([1, 2, 3])
+
+            # call extended method
+            array1.foo("Hello World!")
+
+            # call basic method
+            for i in array1 do
+                puts i
+            end
+            _,
+        );
+
+        $executor = $rubyVMManager
+            ->rubyVM
+            ->disassemble(RubyVersion::VERSION_3_2)
+        ;
+
+        $this->assertSame(ExecutedStatus::SUCCESS, $executor->execute()->executedStatus);
+        $this->assertSame(<<< '_'
+        Hello World!
+        1
+        2
+        3
+
+        _, $rubyVMManager->stdOut->readAll());
+    }
 }
