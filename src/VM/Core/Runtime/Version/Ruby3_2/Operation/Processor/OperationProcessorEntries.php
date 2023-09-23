e6d494ba60b5b7bf3912e558874b0f70d39e90be
m3m0r7 <memory.style.serv@gmail.com>
2023-09-23 21:49:53 +0900

    Refactor

--- src/VM/Core/Runtime/Version/Ruby3_2/Operation/Processor/OperationProcessorEntries.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace RubyVM\VM\Core\Runtime\Version\Ruby3_2\Operation\Processor;
-
-use RubyVM\VM\Core\Runtime\Executor\Operation\DefaultOperationProcessorEntries;
-
-class OperationProcessorEntries extends DefaultOperationProcessorEntries {}
