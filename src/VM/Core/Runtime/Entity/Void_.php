e856eeb22382138a43c2058326adde2a7ffdb04e
m3m0r7 <memory.style.serv@gmail.com>
2023-09-29 22:22:19 +0900

    Use attribute for aliasing class name

--- src/VM/Core/Runtime/Entity/Void_.php
+++ src/VM/Core/Runtime/Entity/Void_.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace RubyVM\VM\Core\Runtime\Entity;
 
+use RubyVM\VM\Core\Runtime\Attribute\BindAliasAs;
 use RubyVM\VM\Core\YARV\Essential\Symbol\VoidSymbol;
 
+#[BindAliasAs('Void')]
 class Void_ extends Entity implements EntityInterface
 {
     public function __construct(VoidSymbol $symbol)
--- src/VM/Core/Runtime/Kernel/Ruby3_2/HeapSpace/DefaultInstanceHeapSpace.php
+++ src/VM/Core/Runtime/Kernel/Ruby3_2/HeapSpace/DefaultInstanceHeapSpace.php
@@ -4,31 +4,47 @@ declare(strict_types=1);
 
 namespace RubyVM\VM\Core\Runtime\Kernel\Ruby3_2\HeapSpace;
 
-use RubyVM\VM\Core\Runtime\Entity\Array_;
+use RubyVM\VM\Core\Runtime\Attribute\BindAliasAs;
 use RubyVM\VM\Core\Runtime\Entity\EntityInterface;
 use RubyVM\VM\Core\Runtime\UserlandHeapSpace;
 
 class DefaultInstanceHeapSpace extends UserlandHeapSpace
 {
     /**
-     * @var array<string, class-string<EntityInterface>>
+     * @var array<string>
      */
-    protected static array $bindClassNames = [
-        'Array' => Array_::class,
+    protected static array $bindClassDirectories = [
+        __DIR__ . '/../../../BasicObject',
+        __DIR__ . '/../../../Entity',
     ];
-
-    /**
-     * @var array{class-string<EntityInterface>[], string, string}[]
-     */
-    protected static array $bindAliasesMethods = [];
 
     public function __construct()
     {
         parent::__construct();
 
-        foreach (static::$bindClassNames as $originalClassName => $bindClassName) {
-            $this->userlandClasses
-                ->alias($originalClassName, $bindClassName);
+        foreach (self::$bindClassDirectories as $directory) {
+            $this->loadClasses($directory);
+        }
+
+        foreach (get_declared_classes() as $className) {
+            $reflection = new \ReflectionClass($className);
+            foreach ($reflection->getAttributes(BindAliasAs::class) as $attribute) {
+                $this->userlandClasses
+                    ->alias($attribute->getArguments()[0], $className);
+            }
+        }
+    }
+
+    protected function loadClasses(string $directory): void
+    {
+        foreach ((glob("{$directory}/*") ?: []) as $file) {
+            if (is_dir($file)) {
+                $this->loadClasses($file);
+
+                continue;
+            }
+
+            require_once $file;
         }
     }
 }
