427626ae44a6d938b8105cd2012adcd449a76d14
m3m0r7 <memory.style.serv@gmail.com>
2023-09-30 12:21:20 +0900

    WIP

--- src/VM/Core/Runtime/BasicObject/BasicObject.php
+++ src/VM/Core/Runtime/BasicObject/BasicObject.php
@@ -7,6 +7,7 @@ namespace RubyVM\VM\Core\Runtime\BasicObject;
 use RubyVM\VM\Core\Runtime\Attribute\BindAliasAs;
 use RubyVM\VM\Core\Runtime\Entity\Class_;
 use RubyVM\VM\Core\Runtime\Entity\EntityInterface;
+use RubyVM\VM\Core\Runtime\Entity\String_;
 use RubyVM\VM\Core\Runtime\Essential\RubyClassInterface;
 use RubyVM\VM\Core\YARV\Criterion\ShouldBeRubyClass;
 use RubyVM\VM\Core\YARV\Essential\Symbol\StringSymbol;
@@ -30,6 +31,13 @@ abstract class BasicObject implements RubyClassInterface
     }
 
     #[BindAliasAs('to_s')]
+    public function toString(): String_
+    {
+        return String_::createBy(
+            (string) $this,
+        );
+    }
+
     public function __toString(): string
     {
         return $this->className();
